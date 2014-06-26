e584a3722cf64649a5c57217e0c7a381a093275d
termie <github@anarkystic.com>
2014-06-26 20:04:20 +0200

    updated env

--- example.py
+++ example.py
@@ -19,73 +19,203 @@ class WerckerConfig(dict):
   """
 
   def __init__(self, data):
+    global_options = GlobalOptions(data)
     self.data = data
-    self.box = WerckerBox(self.data.get('box'))
-    self.services = WerckerServices(self.data.get('services', []))
-    self.build = WerckerBuild(self.data.get('build', {}))
+    self.box = Box(self.data.get('box'))
+    self.services = Services(self.data.get('services', []))
+    self.build = Build(self.data.get('build', {}), global_options)
     self.deploy = WerckerDeploy(self.data.get('deploy', {}))
 
 
-class WerckerBox(dict):
+class Box(dict):
   """Metadata about a box."""
   def __init__(self, data):
     self['name'] = data
     self.name = data
 
 
-class WerckerServices(list):
+class Services(list):
   """Aggregate of wercker services."""
   pass
 
 
-class WerckerService(dict):
-  """Metadata about a service.
-
-  Will include things like download url.
-  """
-  pass
-
-
-class WerckerSteps(list):
-  """Collection of steps."""
-
-  def __init__(self, data):
-    self.steps = [wercker_step_factory(x) for x in data]
+class GlobalOptions(dict):
+  def __init__(self, raw):
+    self['source-dir'] = raw.get('source-dir', '')
+    self['no-response-timeout'] = raw.get('no-response-timeout', 5)
+    self['command-timeout'] = raw.get('command-timeout', 10)
 
 
-def wercker_step_factory(step):
-  """Some steps have special internal implementations, maybe look them up."""
-  return step
-  if stepname in _SPECIAL_STEPS:
-    return _SPECIAL_STEPS[step.name](step)
-  return WerckerStep(step)
+class Step(object):
+  def __init__(self, step_id, data, build):
+    if '/' in step_id:
+      owner, name = step_id.split('/', 2)
+      step_id = '%s_%s' % (owner, name)
+    else:
+      owner = 'wercker'
+      name = step_id
 
+    self._owner = owner
+    self._name = name
 
-class WerckerStep(dict):
-  """Metadata about a step.
-
-  Will include things like, download url.
-  """
-  pass
-
-
-class WerckerBuild(dict):
-  """Collection of build steps."""
-
-  def __init__(self, data):
+    self.id = step_id
     self.data = data
+    self.build = build
+
+  def host_path(self):
+    return os.path.join(self.build.host_root, self.id)
+
+  def guest_path(self):
+    return os.path.join(self.build.guest_root, self.id)
+
+  # TODO(termie): we need to fetch the step
+  def fetch_step(self):
+    pass
+
+  def report_dir(self):
+    return os.path.join(self.build.report_dir(), self.id)
+
+  def report_numbers_file(self):
+    return self.report_dir() + '/numbers.ini'
+
+  def report_message_file(self):
+    return self.report_dir() + '/message.txt'
+
+  def report_artifacts_dir(self):
+    return self.report_dir() + '/artifacts'
+
+  def _get_properties(self):
+    step_yml_path = os.path.join(self.host_path(), 'wercker-step')
+    o = {}
+    if os.path.exists(step_yml_path):
+      step_config = yaml.load(open(step_yml_path))
+      if 'properties' in step_config:
+        for k, v in step_config['properties'].iteritems():
+          if k in self.data:
+            value = self.data[k]
+          else:
+            value = v.get('default', '')
+          o['WERCKER_%s_%s' % (self._name.replace('-', '_'), k)] = value
+    return o
+
+  def _env(self):
+    env = self._get_properties()
+    o = {'WERCKER_STEP_ROOT': self.guest_path(),
+         'WERCKER_STEP_ID': self.id,
+         'WERCKER_STEP_OWNER': self._owner,
+         'WERCKER_STEP_NAME': self._name,
+         'WERCKER_REPORT_NUMBERS_FILE': 'unusued',
+         'WERCKER_REPORT_MESSAGE_FILE': self.report_message_file(),
+         'WERCKER_REPORT_ARTIFACTS_DIR': self.report_artifacts_dir(),
+         }
+    env.update(o)
+    return env
+
+
+class ScriptStep(Step):
+  def __init__(self, data, build):
+    step_id = uuid.uuid4().hex
+    super(ScriptStep, self).__init__(step_id, data, build)
+
+  # NOTE(termie): we overload the fetch method to generate a
+  #               script instead.
+  def fetch_step(self):
+    os.makedirs(self.host_path())
+    script_path = self.host_path() + '/run.sh'
+    content = self._normalize_code(self.data['code'])
+    with open(script_path, 'w') as fp:
+      fp.write(content)
+
+  def _normalize_code(self, s):
+    code = s.split('\n')
+    if not code[0].startswith('#!'):
+      code.insert(0, '#!/bin/bash -xe')
+    return '\n'.join(code)
+
+
+class Build(object):
+  """Collection of build steps and an environment."""
+
+  # Some variables from the current environment will be passed on down
+  # to the container if present.
+  MIRROR_ENV = ['WERCKER_GIT_DOMAIN',
+                'WERCKER_GIT_OWNER',
+                'WERCKER_GIT_REPOSITORY',
+                'WERCKER_GIT_BRANCH',
+                'WERCKER_GIT_COMMIT',
+                'WERCKER_STARTED_BY',
+                'WERCKER_MAIN_PIPELINE_STARTED',
+                'WERCKER_APPLICATION_URL',
+                'WERCKER_APPLICATION_ID',
+                'WERCKER_APPLICATION_NAME',
+                'WERCKER_APPLICATION_OWNER_NAME',
+                ]
+
+  guest_root = '/pipeline'
+
+
+  def __init__(self, data, global_options):
+    self.data = data
+    self.global_options = global_options
     self.steps = self._convert_steps(data.get('steps', []))
 
-  def _convert_steps(self, steps):
-    """Convert [{step_id: step_content}] to [(step_id, step_content)]."""
-    steps_out = [(d.items()[0][0], d.items()[0][1]) for d in steps]
-    steps_out.insert(0, ('wercker-init', {}))
-    return steps_out
 
-  def get_env(self, options):
-    return {'ci': 'True',
-            '_build_id': options.buildId,
-            '_build_url': settings.build_url(options.buildId)}
+  def _convert_steps(self, steps):
+    """Convert [{step_id: step_content}] to WerckerSteps."""
+    steps_list = [(d.items()[0][0], d.items()[0][1]) for d in steps]
+    steps_list.insert(0, ('wercker-init', {}))
+    out = []
+    for step_id, step_content in steps_list:
+      if step_id == 'script':
+        out.append(ScriptStep(step_content))
+      else:
+        out.append(Step(step_id, step_content))
+
+    return out
+
+  def report_dir(self):
+    return self.guest_root + '/report'
+
+  def _id(self):
+    return os.environ.get('WERCKER_BUILD_ID', uuid.uuid4().hex)
+
+  def _env(self):
+    wercker_root = self.guest_root + '/source'
+    env = self._get_passthru_env()
+    env.update(self._get_mirror_env())
+    o = {'WERCKER': 'true',
+         'BUILD': 'true',
+         'CI': 'true',
+         'WERCKER_ROOT': wercker_root,
+         'WERCKER_SOURCE_DIR': os.path.join(
+            wercker_root, self.global_options['source-dir']),
+         'WERCKER_CACHE_DIR': '/cache',
+         'WERCKER_OUTPUT_DIR': self.guest_root + '/output',
+         'WERCKER_PIPELINE_DIR': self.guest_root,
+         'WERCKER_REPORT_DIR': self.report_dir(),
+         }
+    env.update(o)
+    return env
+
+  def _get_mirror_env(self):
+    o = {}
+    for x in self.MIRROR_ENV:
+      if x in os.environ:
+        o[x] = os.environ.get(x)
+
+    return o
+
+  def _get_passthru_env(self):
+    o = {}
+    for k, v in os.environ.iteritems():
+      if k.startswith('PASSTHRU_'):
+        o[k[len('PASSTHRU_'):]] = v
+
+    return o
+
+
+
+
 
 
 class WerckerDeploy(WerckerSteps):
@@ -103,6 +233,13 @@ def get_logger():
 
 
 
+# dev only
+PROJECT_DIR = './projects'
+BUILD_DIR = './builds'
+STEP_DIR = './steps'
+CONTAINER_MNT = '/mnt'
+CONTAINER_TMP = '/tmp'
+
 
 def load_config(path):
   # ...
@@ -113,8 +250,6 @@ def load_config(path):
 def build_environment(config, step):
   """Build up the environment objects."""
   pass
-
-
 
 
 def maybe_pull_image(box, d=None):
@@ -211,12 +346,7 @@ class Session(object):
     while True:
       yield self._recv_queue.get()
 
-# dev only
-PROJECT_DIR = './projects'
-BUILD_DIR = './builds'
-STEP_DIR = './steps'
-CONTAINER_MNT = '/mnt'
-CONTAINER_TMP = '/tmp'
+
 
 # TODO(termie): actually deal with getting code :p
 # TODO(termie): and branches and tags and commits
