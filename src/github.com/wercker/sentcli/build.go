e042846e6c060b4e454c6404a3e04f1f691d66b7
Matthew Hooker <mwhooker@gmail.com>
2014-09-15 16:53:08 -0700

    go fmt

--- src/github.com/wercker/sentcli/build.go
+++ src/github.com/wercker/sentcli/build.go
@@ -1,173 +1,162 @@
 package main
 
 import (
-  "errors"
-  "fmt"
-  "path"
+	"errors"
+	"fmt"
+	"path"
 )
 
-
 type Build struct {
-  Env *Environment
-  Steps []*Step
-  options *GlobalOptions
+	Env     *Environment
+	Steps   []*Step
+	options *GlobalOptions
 }
-
 
 var MirroredEnv = [...]string{"WERCKER_GIT_DOMAIN",
-                              "WERCKER_GIT_OWNER",
-                              "WERCKER_GIT_REPOSITORY",
-                              "WERCKER_GIT_BRANCH",
-                              "WERCKER_GIT_COMMIT",
-                              "WERCKER_STARTED_BY",
-                              "WERCKER_MAIN_PIPELINE_STARTED",
-                              "WERCKER_APPLICATION_URL",
-                              "WERCKER_APPLICATION_ID",
-                              "WERCKER_APPLICATION_NAME",
-                              "WERCKER_APPLICATION_OWNER_NAME"}
-
+	"WERCKER_GIT_OWNER",
+	"WERCKER_GIT_REPOSITORY",
+	"WERCKER_GIT_BRANCH",
+	"WERCKER_GIT_COMMIT",
+	"WERCKER_STARTED_BY",
+	"WERCKER_MAIN_PIPELINE_STARTED",
+	"WERCKER_APPLICATION_URL",
+	"WERCKER_APPLICATION_ID",
+	"WERCKER_APPLICATION_NAME",
+	"WERCKER_APPLICATION_OWNER_NAME"}
 
 func (b *RawBuild) ToBuild(options *GlobalOptions) (*Build, error) {
-  var steps []*Step
-  var build Build
+	var steps []*Step
+	var build Build
 
-  // Start with the secret step, wercker-init that runs before everything
-  rawStepData := RawStepData{}
-  initStep, err := CreateStep("wercker-init", rawStepData, &build, options)
-  if err != nil {
-    return nil, err
-  }
-  steps = append(steps, initStep)
+	// Start with the secret step, wercker-init that runs before everything
+	rawStepData := RawStepData{}
+	initStep, err := CreateStep("wercker-init", rawStepData, &build, options)
+	if err != nil {
+		return nil, err
+	}
+	steps = append(steps, initStep)
 
-  for _, extraRawStep := range b.RawSteps {
-    rawStep, err := NormalizeStep(extraRawStep)
-    if err != nil {
-      return nil, err
-    }
-    step, err := rawStep.ToStep(&build, options)
-    if err != nil {
-      return nil, err
-    }
-    steps = append(steps, step)
-  }
+	for _, extraRawStep := range b.RawSteps {
+		rawStep, err := NormalizeStep(extraRawStep)
+		if err != nil {
+			return nil, err
+		}
+		step, err := rawStep.ToStep(&build, options)
+		if err != nil {
+			return nil, err
+		}
+		steps = append(steps, step)
+	}
 
-  build.options = options
-  build.Steps = steps
+	build.options = options
+	build.Steps = steps
 
-  id, ok := build.options.Env.Map["WERCKER_BUILD_ID"]
-  if ok {
-    build.options.BuildId = id
-  }
+	id, ok := build.options.Env.Map["WERCKER_BUILD_ID"]
+	if ok {
+		build.options.BuildId = id
+	}
 
-  build.InitEnv()
+	build.InitEnv()
 
-  return &build, nil
+	return &build, nil
 }
-
 
 func (b *Build) InitEnv() {
-  b.Env = &Environment{}
-  // TODO(termie): deal with PASSTHRU args from the user here
-  b.Env.Update(b.getMirrorEnv())
+	b.Env = &Environment{}
+	// TODO(termie): deal with PASSTHRU args from the user here
+	b.Env.Update(b.getMirrorEnv())
 
-  // Add all of our basic env vars
-  m := map[string]string {
-    "WERCKER": "true",
-    "BUILD": "true",
-    "CI": "true",
-    "WERCKER_BUILD_ID": b.options.BuildId,
-    "WERCKER_ROOT": b.GuestPath("source"),
-    "WERCKER_SOURCE_DIR": b.GuestPath("source", b.options.SourceDir),
-    "WERCKER_CACHE_DIR": "/cache",
-    "WERCKER_OUTPUT_DIR": b.GuestPath("output"),
-    "WERCKER_PIPELINE_DIR": b.GuestPath(),
-    "WERCKER_REPORT_DIR": b.GuestPath("report"),
-    "TERM": "xterm-256color",
-  }
-  b.Env.Update(m)
+	// Add all of our basic env vars
+	m := map[string]string{
+		"WERCKER":              "true",
+		"BUILD":                "true",
+		"CI":                   "true",
+		"WERCKER_BUILD_ID":     b.options.BuildId,
+		"WERCKER_ROOT":         b.GuestPath("source"),
+		"WERCKER_SOURCE_DIR":   b.GuestPath("source", b.options.SourceDir),
+		"WERCKER_CACHE_DIR":    "/cache",
+		"WERCKER_OUTPUT_DIR":   b.GuestPath("output"),
+		"WERCKER_PIPELINE_DIR": b.GuestPath(),
+		"WERCKER_REPORT_DIR":   b.GuestPath("report"),
+		"TERM":                 "xterm-256color",
+	}
+	b.Env.Update(m)
 }
-
 
 func (b *Build) getMirrorEnv() map[string]string {
-  var m = make(map[string]string)
-  for _, key := range MirroredEnv {
-    value, ok := b.options.Env.Map[key]
-    if ok {
-      m[key] = value
-    }
-  }
-  return m
+	var m = make(map[string]string)
+	for _, key := range MirroredEnv {
+		value, ok := b.options.Env.Map[key]
+		if ok {
+			m[key] = value
+		}
+	}
+	return m
 }
-
 
 func (b *Build) SourcePath() string {
-  return b.GuestPath("source", b.options.SourceDir)
+	return b.GuestPath("source", b.options.SourceDir)
 }
 
-
-func (b *Build) HostPath(s ...string) (string) {
-  hostPath := path.Join(b.options.BuildDir, b.options.BuildId)
-  for _, v := range s {
-    hostPath = path.Join(hostPath, v)
-  }
-  return hostPath
+func (b *Build) HostPath(s ...string) string {
+	hostPath := path.Join(b.options.BuildDir, b.options.BuildId)
+	for _, v := range s {
+		hostPath = path.Join(hostPath, v)
+	}
+	return hostPath
 }
 
-
-func (b *Build) GuestPath(s ...string) (string) {
-  guestPath := b.options.GuestRoot
-  for _, v := range s {
-    guestPath = path.Join(guestPath, v)
-  }
-  return guestPath
+func (b *Build) GuestPath(s ...string) string {
+	guestPath := b.options.GuestRoot
+	for _, v := range s {
+		guestPath = path.Join(guestPath, v)
+	}
+	return guestPath
 }
 
-
-func (b *Build) MntPath(s ...string) (string) {
-  mntPath := b.options.MntRoot
-  for _, v := range s {
-    mntPath = path.Join(mntPath, v)
-  }
-  return mntPath
+func (b *Build) MntPath(s ...string) string {
+	mntPath := b.options.MntRoot
+	for _, v := range s {
+		mntPath = path.Join(mntPath, v)
+	}
+	return mntPath
 }
 
-
-func (b *Build) ReportPath(s ...string) (string) {
-  reportPath := b.options.ReportRoot
-  for _, v := range s {
-    reportPath = path.Join(reportPath, v)
-  }
-  return reportPath
+func (b *Build) ReportPath(s ...string) string {
+	reportPath := b.options.ReportRoot
+	for _, v := range s {
+		reportPath = path.Join(reportPath, v)
+	}
+	return reportPath
 }
-
 
 func (b *Build) SetupGuest(sess *Session) error {
-  // Make sure our guest path exists
-  exit, _, err := sess.SendChecked(fmt.Sprintf(`mkdir "%s"`, b.GuestPath()))
-  if err != nil {
-    return err
-  }
-  if exit != 0 {
-    return errors.New("Guest command failed.")
-  }
+	// Make sure our guest path exists
+	exit, _, err := sess.SendChecked(fmt.Sprintf(`mkdir "%s"`, b.GuestPath()))
+	if err != nil {
+		return err
+	}
+	if exit != 0 {
+		return errors.New("Guest command failed.")
+	}
 
-  // And the cache path
-  exit, _, err = sess.SendChecked(fmt.Sprintf(`mkdir "%s"`, "/cache"))
-  if err != nil {
-    return err
-  }
-  if exit != 0 {
-    return errors.New("Guest command failed.")
-  }
+	// And the cache path
+	exit, _, err = sess.SendChecked(fmt.Sprintf(`mkdir "%s"`, "/cache"))
+	if err != nil {
+		return err
+	}
+	if exit != 0 {
+		return errors.New("Guest command failed.")
+	}
 
-  // Copy the source dir to the guest path
-  exit, _, err = sess.SendChecked(fmt.Sprintf(`cp -r "%s" "%s"`, b.MntPath("source"), b.GuestPath("source")))
-  if err != nil {
-    return err
-  }
-  if exit != 0 {
-    return errors.New("Guest command failed.")
-  }
+	// Copy the source dir to the guest path
+	exit, _, err = sess.SendChecked(fmt.Sprintf(`cp -r "%s" "%s"`, b.MntPath("source"), b.GuestPath("source")))
+	if err != nil {
+		return err
+	}
+	if exit != 0 {
+		return errors.New("Guest command failed.")
+	}
 
-  return nil
+	return nil
 }
--- src/github.com/wercker/sentcli/session.go
+++ src/github.com/wercker/sentcli/session.go
@@ -1,98 +1,92 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "strings"
-  "code.google.com/p/go.net/websocket"
-  "code.google.com/p/go-uuid/uuid"
+	"code.google.com/p/go-uuid/uuid"
+	"code.google.com/p/go.net/websocket"
+	"fmt"
+	"log"
+	"strings"
 )
-
 
 // Session is our class for interacting with a running Docker container.
 type Session struct {
-  wsUrl string
-  ws *websocket.Conn
-  ch chan string
+	wsUrl string
+	ws    *websocket.Conn
+	ch    chan string
 }
-
 
 // CreateSession based on a docker api endpoint and container ID.
-func CreateSession (endpoint string, containerID string) (*Session) {
-  wsEndpoint := strings.Replace(endpoint, "tcp://", "ws://", 1)
-  wsQuery := "stdin=1&stderr=1&stdout=1&stream=1"
-  wsUrl := fmt.Sprintf("%s/containers/%s/attach/ws?%s",
-                       wsEndpoint, containerID, wsQuery)
+func CreateSession(endpoint string, containerID string) *Session {
+	wsEndpoint := strings.Replace(endpoint, "tcp://", "ws://", 1)
+	wsQuery := "stdin=1&stderr=1&stdout=1&stream=1"
+	wsUrl := fmt.Sprintf("%s/containers/%s/attach/ws?%s",
+		wsEndpoint, containerID, wsQuery)
 
-  ch := make(chan string)
+	ch := make(chan string)
 
-  return &Session{wsUrl: wsUrl, ws:nil, ch:ch}
+	return &Session{wsUrl: wsUrl, ws: nil, ch: ch}
 }
-
 
 // ReadToChan reads on a websocket forever, writing to a channel
 func ReadToChan(ws *websocket.Conn, ch chan string) {
-  var data string
-  for {
-    err := websocket.Message.Receive(ws, &data)
-    if err != nil {
-      log.Fatalln(err)
-    }
-    ch <- data
-  }
+	var data string
+	for {
+		err := websocket.Message.Receive(ws, &data)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		ch <- data
+	}
 }
-
 
 // Attach begins reading on the websocket and writing to the internal channel.
 func (s *Session) Attach() (*Session, error) {
-  ws, err := websocket.Dial(s.wsUrl, "", "http://localhost/")
-  if err != nil {
-    return s, err
-  }
-  s.ws = ws
+	ws, err := websocket.Dial(s.wsUrl, "", "http://localhost/")
+	if err != nil {
+		return s, err
+	}
+	s.ws = ws
 
-  go ReadToChan(s.ws, s.ch)
-  return s, nil
+	go ReadToChan(s.ws, s.ch)
+	return s, nil
 }
-
 
 // Send an array of commands.
 func (s *Session) Send(commands ...string) {
-  for i := range commands {
-    fmt.Println("send: ", commands[i])
-    err := websocket.Message.Send(s.ws, commands[i] + "\n")
-    if err != nil {
-      log.Fatalln(err)
-    }
-  }
+	for i := range commands {
+		fmt.Println("send: ", commands[i])
+		err := websocket.Message.Send(s.ws, commands[i]+"\n")
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
-
 
 // SendChecked sends commands, waits for them to complete and returns the
 // exit status and output
 func (s *Session) SendChecked(commands ...string) (int, []string, error) {
-  var exitCode int
-  rand := uuid.NewRandom().String()
-  check := false
-  recv := []string{}
+	var exitCode int
+	rand := uuid.NewRandom().String()
+	check := false
+	recv := []string{}
 
-  s.Send(commands...)
-  s.Send(fmt.Sprintf("echo %s $?", rand))
+	s.Send(commands...)
+	s.Send(fmt.Sprintf("echo %s $?", rand))
 
-  // This is relatively naive and will break if the messages returned aren't
-  // complete lines, if this becomes a problem we'll have to buffer it.
-  for check != true {
-    line := <- s.ch
-    fmt.Print("recv: ", line)
-    if strings.HasPrefix(line, rand) {
-      check = true
-      _, err := fmt.Sscanf(line, "%s %d\n", &rand, &exitCode)
-      if err != nil {
-        return exitCode, recv, err
-      }
-    } else {
-      recv = append(recv, line)
-    }
-  }
-  return exitCode, recv, nil
+	// This is relatively naive and will break if the messages returned aren't
+	// complete lines, if this becomes a problem we'll have to buffer it.
+	for check != true {
+		line := <-s.ch
+		fmt.Print("recv: ", line)
+		if strings.HasPrefix(line, rand) {
+			check = true
+			_, err := fmt.Sscanf(line, "%s %d\n", &rand, &exitCode)
+			if err != nil {
+				return exitCode, recv, err
+			}
+		} else {
+			recv = append(recv, line)
+		}
+	}
+	return exitCode, recv, nil
 }
