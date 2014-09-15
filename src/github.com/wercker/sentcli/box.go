09d5d22a276cfbe959d6092965ac16b0c76651c3
Matthew Hooker <mwhooker@gmail.com>
2014-09-15 16:48:50 -0700

    Grab services from yaml and start logging them.

--- src/github.com/wercker/sentcli/box.go
+++ src/github.com/wercker/sentcli/box.go
@@ -1,54 +1,50 @@
 package main
 
 import (
-  "reflect"
-  "strings"
-  "github.com/fsouza/go-dockerclient"
+	"github.com/fsouza/go-dockerclient"
+	"reflect"
+	"strings"
 )
 
-
 type BoxConfig struct {
-  Env map[string]string
-  Name string
-  Owner string
-  Version string
+	Env     map[string]string
+	Name    string
+	Owner   string
+	Version string
 }
-
 
 type Box struct {
-  Name string
-  build *Build
-  options *GlobalOptions
+	Name    string
+	build   *Build
+	options *GlobalOptions
 }
-
 
 // Convert RawBox into a Box
 func (b *RawBox) ToBox(build *Build, options *GlobalOptions) (*Box, error) {
-  v := reflect.ValueOf(*b)
-  return CreateBox(v.String(), build, options)
+	v := reflect.ValueOf(*b)
+	return CreateBox(v.String(), build, options)
 }
-
 
 // CreateBox from a name and other references
 func CreateBox(name string, build *Build, options *GlobalOptions) (*Box, error) {
-  // TODO(termie): right now I am just tacking the version into the name
-  //               by replacing @ with _
-  name = strings.Replace(name, "@", "_", 1)
-  return &Box{Name: name, build: build, options: options}, nil
+	// TODO(termie): right now I am just tacking the version into the name
+	//               by replacing @ with _
+	name = strings.Replace(name, "@", "_", 1)
+	return &Box{Name: name, build: build, options: options}, nil
 }
-
 
 // Fetch an image if we don't have it already
 // TODO(termie): we don't actually fetch new ones yet!
 func (b *Box) Fetch() (*docker.Image, error) {
-  client, err := docker.NewClient(b.options.DockerEndpoint)
-  if err != nil {
-    panic(err)
-  }
+	client, err := docker.NewClient(b.options.DockerEndpoint)
+	if err != nil {
+		panic(err)
+	}
 
-  image, err := client.InspectImage(b.Name)
-  if err == nil {
-    return image, nil
-  }
-  return nil, err
+	image, err := client.InspectImage(b.Name)
+	if err == nil {
+		return image, nil
+	}
+	// TODO (mh): "oh, no image? try a docker pull"
+	return nil, err
 }
--- src/github.com/wercker/sentcli/sentcli.go
+++ src/github.com/wercker/sentcli/sentcli.go
@@ -1,214 +1,213 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "log"
-  "os"
-  "github.com/codegangsta/cli"
-  "github.com/fsouza/go-dockerclient"
-  "github.com/termie/go-shutil"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"github.com/fsouza/go-dockerclient"
+	"github.com/termie/go-shutil"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
-
 func main() {
-  app := cli.NewApp()
-  app.Flags = []cli.Flag {
-    cli.StringFlag{Name:"projectDir", Value:"./projects", Usage:"path where projects live"},
-    cli.StringFlag{Name:"stepDir", Value:"./steps", Usage:"path where steps live"},
-    cli.StringFlag{Name:"buildDir", Value:"./builds", Usage:"path where builds live"},
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{Name: "projectDir", Value: "./projects", Usage: "path where projects live"},
+		cli.StringFlag{Name: "stepDir", Value: "./steps", Usage: "path where steps live"},
+		cli.StringFlag{Name: "buildDir", Value: "./builds", Usage: "path where builds live"},
 
-    cli.StringFlag{Name:"dockerEndpoint", Value:"tcp://127.0.0.1:2375", Usage:"docker api endpoint"},
-    cli.StringFlag{Name:"werckerEndpoint", Value:"https://app.wercker.com/api/v2", Usage:"wercker api endpoint"},
-    cli.StringFlag{Name:"mntRoot", Value:"/mnt", Usage:"directory on the guest where volumes are mounted"},
-    cli.StringFlag{Name:"guestRoot", Value:"/pipeline", Usage:"directory on the guest where work is done"},
-    cli.StringFlag{Name:"reportRoot", Value:"/report", Usage:"directory on the guest where reports will be written"},
-    cli.StringFlag{Name:"buildId", Value:"", Usage:"build id"},
+		cli.StringFlag{Name: "dockerEndpoint", Value: "tcp://127.0.0.1:2375", Usage: "docker api endpoint"},
+		cli.StringFlag{Name: "werckerEndpoint", Value: "https://app.wercker.com/api/v2", Usage: "wercker api endpoint"},
+		cli.StringFlag{Name: "mntRoot", Value: "/mnt", Usage: "directory on the guest where volumes are mounted"},
+		cli.StringFlag{Name: "guestRoot", Value: "/pipeline", Usage: "directory on the guest where work is done"},
+		cli.StringFlag{Name: "reportRoot", Value: "/report", Usage: "directory on the guest where reports will be written"},
+		cli.StringFlag{Name: "buildId", Value: "", Usage: "build id"},
 
-    // These options might be overwritten by the wercker.yml
-    cli.StringFlag{Name:"sourceDir", Value:"", Usage:"source path relative to checkout root"},
-    cli.IntFlag{Name:"noResponseTimeout", Value:5, Usage:"timeout if no script output is received in this many minutes"},
-    cli.IntFlag{Name:"commandTimeout", Value:10, Usage:"timeout if command does not complete in this many minutes"},
-  }
+		// These options might be overwritten by the wercker.yml
+		cli.StringFlag{Name: "sourceDir", Value: "", Usage: "source path relative to checkout root"},
+		cli.IntFlag{Name: "noResponseTimeout", Value: 5, Usage: "timeout if no script output is received in this many minutes"},
+		cli.IntFlag{Name: "commandTimeout", Value: 10, Usage: "timeout if command does not complete in this many minutes"},
+	}
 
-  app.Commands = []cli.Command{
-    {
-      Name: "build",
-      ShortName: "b",
-      Usage: "build a project",
-      Action: func(c *cli.Context) {
-          BuildProject(c)
-      },
-      Flags: []cli.Flag {
-      },
-    },
-  }
-  app.Run(os.Args)
+	app.Commands = []cli.Command{
+		{
+			Name:      "build",
+			ShortName: "b",
+			Usage:     "build a project",
+			Action: func(c *cli.Context) {
+				BuildProject(c)
+			},
+			Flags: []cli.Flag{},
+		},
+	}
+	app.Run(os.Args)
 }
 
-
 func BuildProject(c *cli.Context) {
-  log.Println("############# Building project #############")
-  log.Println(c.Args().First())
-  log.Println("############################################")
+	log.Println("############# Building project #############")
+	log.Println(c.Args().First())
+	log.Println("############################################")
 
-  // Parse CLI and local env
-  options, err := CreateGlobalOptions(c, os.Environ())
-  if err != nil {
-    log.Panicln(err)
-  }
-  // log.Println(options)
+	// Parse CLI and local env
+	options, err := CreateGlobalOptions(c, os.Environ())
+	if err != nil {
+		log.Panicln(err)
+	}
+	// log.Println(options)
 
-  // Setup a docker client
-  client, _ := docker.NewClient(options.DockerEndpoint)
+	// Setup a docker client
+	client, _ := docker.NewClient(options.DockerEndpoint)
 
-  // NOTE(termie): For now we are expecting it to be downloaded
-  // before we start so we are just expecting it to exist in our
-  // projects directory.
-  project := c.Args().First()
-  projectDir := fmt.Sprintf("%s/%s", options.ProjectDir, project)
+	// NOTE(termie): For now we are expecting it to be downloaded
+	// before we start so we are just expecting it to exist in our
+	// projects directory.
+	project := c.Args().First()
+	projectDir := fmt.Sprintf("%s/%s", options.ProjectDir, project)
 
-  // Return a []byte of the yaml we find or create.
-  werckerYaml, err := ReadWerckerYaml([]string{projectDir}, false)
-  if err != nil {
-    log.Panicln(err)
-  }
+	// Return a []byte of the yaml we find or create.
+	werckerYaml, err := ReadWerckerYaml([]string{projectDir}, false)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  // Parse that bad boy.
-  rawConfig, err := ConfigFromYaml(werckerYaml)
-  if err != nil {
-    log.Panicln(err)
-  }
+	// Parse that bad boy.
+	rawConfig, err := ConfigFromYaml(werckerYaml)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  // Add some options to the global config
-  if rawConfig.SourceDir != "" {
-    options.SourceDir = rawConfig.SourceDir
-  }
+	// Add some options to the global config
+	if rawConfig.SourceDir != "" {
+		options.SourceDir = rawConfig.SourceDir
+	}
 
-  // Promote the RawBuild to a real Build. We believe in you, Build!
-  build, err := rawConfig.RawBuild.ToBuild(options)
-  if err != nil {
-    log.Panicln(err)
-  }
+	// Promote the RawBuild to a real Build. We believe in you, Build!
+	build, err := rawConfig.RawBuild.ToBuild(options)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  // Promote RawBox to a real Box. We believe in you, Box!
-  box, err := rawConfig.RawBox.ToBox(build, options)
-  if err != nil {
-    log.Panicln(err)
-  }
+	// Promote RawBox to a real Box. We believe in you, Box!
+	box, err := rawConfig.RawBox.ToBox(build, options)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  log.Println("Box:", box.Name)
-  log.Println("Steps:", len(build.Steps))
+	log.Println("Box:", box.Name)
+	log.Println("Steps:", len(build.Steps))
 
-  // Make sure we have the box available
-  image, err := box.Fetch()
-  if err != nil {
-    log.Panicln(err)
-  }
+	// Make sure we have the box available
+	image, err := box.Fetch()
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  log.Println("Docker Image:", image.ID)
+	log.Println("Docker Image:", image.ID)
 
+	for _, service := range rawConfig.RawServices {
+		log.Println("Fetching service:", service)
+	}
+	log.Panicln("Services:", rawConfig.RawServices)
 
-  // TODO(termie): Services go here
+	// Start setting up the build dir
+	err = os.MkdirAll(build.HostPath(), 0755)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  // Start setting up the build dir
-  err = os.MkdirAll(build.HostPath(), 0755)
-  if err != nil {
-    log.Panicln(err)
-  }
+	err = shutil.CopyTree(projectDir, build.HostPath("source"), nil)
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  err = shutil.CopyTree(projectDir, build.HostPath("source"), nil)
-  if err != nil {
-    log.Panicln(err)
-  }
+	// Make sure we have the steps
+	for _, step := range build.Steps {
+		log.Println("Fetching Step:", step.Name, step.Id)
+		_, err := step.Fetch()
+		if err != nil {
+			log.Panicln(err)
+		}
+	}
 
-  // Make sure we have the steps
-  for _, step := range build.Steps {
-    log.Println("Fetching Step:", step.Name, step.Id)
-    _, err := step.Fetch()
-    if err != nil {
-      log.Panicln(err)
-    }
-  }
+	// Make our list of binds for the Docker attach
+	// NOTE(termie): we don't appear to need the "volumes" stuff, leaving
+	//               it commented out in case it actually does something
+	binds := []string{}
+	// volumes := make(map[string]struct{})
+	entries, err := ioutil.ReadDir(build.HostPath())
+	for _, entry := range entries {
+		if entry.IsDir() {
+			binds = append(binds, fmt.Sprintf("%s:%s:ro", build.HostPath(entry.Name()), build.MntPath(entry.Name())))
+			// volumes[build.MntPath(entry.Name())] = struct{}{}
+		}
+	}
 
-  // Make our list of binds for the Docker attach
-  // NOTE(termie): we don't appear to need the "volumes" stuff, leaving
-  //               it commented out in case it actually does something
-  binds := []string{}
-  // volumes := make(map[string]struct{})
-  entries, err := ioutil.ReadDir(build.HostPath())
-  for _, entry := range entries {
-    if entry.IsDir() {
-      binds = append(binds, fmt.Sprintf("%s:%s:ro", build.HostPath(entry.Name()), build.MntPath(entry.Name())))
-      // volumes[build.MntPath(entry.Name())] = struct{}{}
-    }
-  }
+	// Make and start the container
+	containerName := "wercker-build-" + options.BuildId
+	container, err := client.CreateContainer(
+		docker.CreateContainerOptions{
+			Name: containerName,
+			Config: &docker.Config{
+				Image:        box.Name,
+				Tty:          false,
+				OpenStdin:    true,
+				Cmd:          []string{"/bin/bash"},
+				AttachStdin:  true,
+				AttachStdout: true,
+				AttachStderr: true,
+				// Volumes: volumes,
+			},
+		})
+	if err != nil {
+		log.Panicln(err)
+	}
 
-  // Make and start the container
-  containerName := "wercker-build-" + options.BuildId
-  container, err := client.CreateContainer(
-    docker.CreateContainerOptions{
-      Name: containerName,
-      Config: &docker.Config{
-        Image: box.Name,
-        Tty: false,
-        OpenStdin: true,
-        Cmd: []string{"/bin/bash"},
-        AttachStdin: true,
-        AttachStdout: true,
-        AttachStderr: true,
-        // Volumes: volumes,
-      },
-  })
-  if err != nil {
-    log.Panicln(err)
-  }
+	log.Println("Docker Container:", container.ID)
+	client.StartContainer(container.ID, &docker.HostConfig{Binds: binds})
 
-  log.Println("Docker Container:", container.ID)
-  client.StartContainer(container.ID, &docker.HostConfig{Binds:binds})
+	// Start our session
+	sess := CreateSession(options.DockerEndpoint, container.ID)
+	sess, err = sess.Attach()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  // Start our session
-  sess := CreateSession(options.DockerEndpoint, container.ID)
-  sess, err = sess.Attach()
-  if err != nil {
-    log.Fatalln(err)
-  }
+	// Some helpful logging
+	log.Println("Base Build Environment:")
+	for k, v := range build.Env.Map {
+		log.Println(" ", k, v)
+	}
 
-  // Some helpful logging
-  log.Println("Base Build Environment:")
-  for k, v := range build.Env.Map {
-    log.Println(" ", k, v)
-  }
+	err = build.SetupGuest(sess)
+	exit, _, err := sess.SendChecked(build.Env.Export()...)
+	if err != nil {
+		log.Panicln(err)
+	}
+	if exit != 0 {
+		log.Fatalln("Build failed with exit code:", exit)
+	}
 
-  err = build.SetupGuest(sess)
-  exit, _, err := sess.SendChecked(build.Env.Export()...)
-  if err != nil {
-    log.Panicln(err)
-  }
-  if exit != 0 {
-    log.Fatalln("Build failed with exit code:", exit)
-  }
+	for _, step := range build.Steps {
+		log.Println()
+		log.Println("============= Executing Step ==============")
+		log.Println(step.Name, step.Id)
+		log.Println("===========================================")
 
-  for _, step := range build.Steps {
-    log.Println()
-    log.Println("============= Executing Step ==============")
-    log.Println(step.Name, step.Id)
-    log.Println("===========================================")
+		step.InitEnv()
+		log.Println("Step Environment")
+		for k, v := range step.Env.Map {
+			log.Println(" ", k, v)
+		}
+		exit, err = step.Execute(sess)
+		if exit != 0 {
+			log.Fatalln("Build failed with exit code:", exit)
+		}
+		if err != nil {
+			log.Panicln(err)
+		}
+		log.Println("============ Step successful! =============")
+	}
 
-    step.InitEnv()
-    log.Println("Step Environment")
-    for k, v := range step.Env.Map {
-      log.Println(" ", k, v)
-    }
-    exit, err = step.Execute(sess)
-    if exit != 0 {
-      log.Fatalln("Build failed with exit code:", exit)
-    }
-    if err != nil {
-      log.Panicln(err)
-    }
-    log.Println("============ Step successful! =============")
-  }
-
-  log.Println("########### Build successful! #############")
+	log.Println("########### Build successful! #############")
 }
