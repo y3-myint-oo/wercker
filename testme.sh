a35de4b2b6b29d966e6cab5d09620167fc491726
Matthew Hooker <mwhooker@gmail.com>
2014-09-18 15:31:54 -0700

    some integration tests that pass and fail.
    pass test uses links.
    modified testme.sh to exercise these tests.

--- testme.sh
+++ testme.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
-./buildme.sh && ./sentcli build $1
+
+function integration_tests() {
+    ./sentcli --projectDir=tests/projects build pass || return 1
+    ./sentcli --projectDir=tests/projects build fail && return 1
+    return 0
+}
+
+./buildme.sh && ./sentcli build $1 && integration_tests
+
+passfail=$?
+
+case $passfail in
+    0)
+        echo "tests pass"
+        ;;
+    1)
+        echo "tests fail"
+        ;;
+esac
+
+exit $passfail
 
 # ./sentcli run "while true; do date; sleep 1; done"
