592bc7a4b464df2c91ab381fab2c7753687d17ad
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2015-01-20 14:36:13 +0100

    deleted unused functions

--- util.go
+++ util.go
@@ -143,19 +143,3 @@ func askForConfirmation() bool {
 		return askForConfirmation()
 	}
 }
-
-// posString returns the first index of element in slice.
-// If slice does not contain element, returns -1.
-func posString(slice []string, element string) int {
-	for index, elem := range slice {
-		if elem == element {
-			return index
-		}
-	}
-	return -1
-}
-
-// containsString returns true iff slice contains element
-func containsString(slice []string, element string) bool {
-	return !(posString(slice, element) == -1)
-}
