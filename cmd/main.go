c06d6cba5cdb71b71ea1e9133aae1d205bf88cdb
akupila <akupila@gmail.com>
2016-05-17 11:53:33 +0200

    cleanup old builds, keep latest 2

--- cmd/main.go
+++ cmd/main.go
@@ -899,6 +899,13 @@ func executePipeline(cmdCtx context.Context, options *core.PipelineOptions, dock
 		})
 		return nil, soft.Exit(err)
 	}
+	err = r.CleanupOldBuilds()
+	if err != nil {
+		e.Emit(core.Logs, &core.LogsArgs{
+			Stream: "stderr",
+			Logs:   err.Error() + "\n",
+		})
+	}
 	if options.Verbose {
 		logger.Printf(f.Success("Copied working dir", timer.String()))
 	}
