c770df332ecbbe9c52db6b996f8355fe839f37ee
Toby James <toby.james@oracle.com>
2017-08-31 12:59:05 +0100

    Refactoring wercker docker tests

--- cmd/docker_test.go
+++ cmd/docker_test.go
@@ -22,33 +22,40 @@ func TestDockerSuite(t *testing.T) {
 	suite.Run(t, suiteTester)
 }
 
-func (s *DockerSuite) TestEnsureWerckerCredentialsNoToken() {
-	opts := &core.WerckerDockerOptions{
+func testWerckerDockerOpts(authToken string, containerRegistry string) *core.WerckerDockerOptions {
+	containerRegistryUrl, _ := url.Parse(containerRegistry)
+	return &core.WerckerDockerOptions{
 		GlobalOptions: &core.GlobalOptions{
-			AuthToken: "",
+			AuthToken: authToken,
 		},
+		WerckerContainerRegistry: containerRegistryUrl,
 	}
-
-	err := ensureWerckerCredentials(opts)
-	s.Equal(errNoWerckerToken, err, "errNoWerckerToken was not returned when token was not present")
 }
 
-func (s *DockerSuite) TestEnsureWerckerCredentialsTokenNoConfig() {
-	testWerckerRegistry, _ := url.Parse("")
-
-	opts := &core.WerckerDockerOptions{
-		GlobalOptions: &core.GlobalOptions{
-			AuthToken: "1234",
-		},
-		WerckerContainerRegistry: testWerckerRegistry,
-	}
-
+func setupTempDockerConfig(s *DockerSuite) error {
 	tempDir := s.WorkingDir()
 	config.SetDir(tempDir)
 	filename := path.Join(tempDir, "config.json")
 	data := []byte("{}")
 
 	err := ioutil.WriteFile(filename, data, 0644)
+	return err
+}
+
+func (s *DockerSuite) TestEnsureWerckerCredentialsNoToken() {
+	authToken := ""
+	opts := testWerckerDockerOpts(authToken, "")
+
+	err := ensureWerckerCredentials(opts)
+	s.Equal(errNoWerckerToken, err, "errNoWerckerToken was not returned when token was not present")
+}
+
+func (s *DockerSuite) TestEnsureWerckerCredentialsTokenNoConfig() {
+	testWerckerRegistry := "test.wcr.io"
+	authToken := "1234"
+	opts := testWerckerDockerOpts(authToken, testWerckerRegistry)
+
+	err := setupTempDockerConfig(s)
 	if err != nil {
 		s.Fail(err.Error(), "Failed to write docker config file")
 	}
@@ -59,21 +66,11 @@ func (s *DockerSuite) TestEnsureWerckerCredentialsTokenNoConfig() {
 }
 
 func (s *DockerSuite) TestEnsureWerckerCredentialsWithDockerConfig() {
-	testWerckerRegistry, _ := url.Parse("")
+	testWerckerRegistry := "test.wcr.io"
+	authToken := "1234"
+	opts := testWerckerDockerOpts(authToken, testWerckerRegistry)
 
-	opts := &core.WerckerDockerOptions{
-		GlobalOptions: &core.GlobalOptions{
-			AuthToken: "1234",
-		},
-		WerckerContainerRegistry: testWerckerRegistry,
-	}
-
-	tempDir := s.WorkingDir()
-	config.SetDir(tempDir)
-	filename := path.Join(tempDir, "config.json")
-	data := []byte("{}")
-
-	err := ioutil.WriteFile(filename, data, 0644)
+	err := setupTempDockerConfig(s)
 	if err != nil {
 		s.Fail(err.Error(), "Failed to write docker config file")
 	}
